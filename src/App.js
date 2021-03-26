3c4b9e1b0affc12e78981ddb7b8e23a1bb14fde4
Sean Russell <seanrussellphotography@gmail.com>
2021-03-26 17:55:49 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 
+/* 
+  This is the top of retroboard app
+  Requirements:
+  1) What went well
+  2) To imporove
+  3) Actions items
+
+  Components:
+  1) Navbar?
+  2) RetroBoard
+  3) StoryCard
+
+*/
+
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1> Retro Board </h1>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
+      
     </div>
   );
 }
