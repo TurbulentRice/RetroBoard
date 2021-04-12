18e895409bb2beb9037371119b8c746886486061
Sean Russell <seanrussellphotography@gmail.com>
2021-04-12 18:14:04 -0400

    moved examples to helpers.js

--- src/contexts/StoriesContext.jsx
+++ src/contexts/StoriesContext.jsx
@@ -1,53 +1,22 @@
 import { createContext, useState } from 'react';
-import { getUID, getNow } from '../lib/helpers';
+import { getTestStories } from '../lib/helpers';
 
 export const StoriesContext = createContext();
 
 export function StoriesProvider({ children }) {
   // Keep track of all stories in single array of objects, no inherent order, like bucket
   // Components will "pick out" stories relevant to them based on state/id
-  const [stories, setStories] = useState([
-    {
-      title: "Get new access token",
-      id: getUID(),
-      owner: "Louise Belcher",
-      description: "Fix issue with multi-factor authentication",
-      points: 3,
-      col: 1,
-      created: getNow()
-    },
-    {
-      title: "Push code to production",
-      id: getUID(),
-      owner: "Louise Belcher",
-      description: "Commit new changes and deploy to production server.",
-      points: 2,
-      col: 2,
-      created: getNow()
-    },
-    {
-      title: "Fix major security issue",
-      id: getUID(),
-      owner: "A Person",
-      description: "Get that thing done",
-      points: 6,
-      col: 3,
-      created: getNow()
-    },
-    {
-      title: "Fix major security issue",
-      id: getUID(),
-      owner: "A Person",
-      description: "Get that thing done",
-      points: 6,
-      col: 3,
-      created: getNow()
-    },
-  ]);
-
+  // Function to remove one card with all matching fields
+  const [stories, setStories] = useState(getTestStories());
+  
   return (
     <StoriesContext.Provider value={{stories, setStories}}>
       {children}
     </StoriesContext.Provider>
   )
 }
+
+
+
+
+
