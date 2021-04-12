15e214e08e408665995e6634b3d35ef785dffc4f
Sean Russell <seanrussellphotography@gmail.com>
2021-04-12 18:13:21 -0400

    spawn examples on page load

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -10,7 +10,7 @@ export const getUID = () => Math.floor(100000 + Math.random() * 900000);
 export const getNow = () => new Date().toDateString();
 
 // Return a new "empty" story object
-// Will overwrite default with anything provided in options obj
+// Will overwrite template with anything provided in options obj
 export const getNewStory = (options) => ({
   title: "",
   id: getUID(),
@@ -21,3 +21,42 @@ export const getNewStory = (options) => ({
   created: getNow(),
   ...options
 })
+
+export const getTestStories = () => [
+  {
+    title: "Get new access token",
+    id: getUID(),
+    owner: "Louise Belcher",
+    description: "Fix issue with multi-factor authentication",
+    points: 3,
+    col: 1,
+    created: getNow()
+  },
+  {
+    title: "Push code to production",
+    id: getUID(),
+    owner: "Louise Belcher",
+    description: "Commit new changes and deploy to production server.",
+    points: 2,
+    col: 2,
+    created: getNow()
+  },
+  {
+    title: "Fix major security issue",
+    id: getUID(),
+    owner: "A Person",
+    description: "Get that thing done",
+    points: 6,
+    col: 3,
+    created: getNow()
+  },
+  {
+    title: "Fix major security issue",
+    id: getUID(),
+    owner: "A Person",
+    description: "Get that thing done",
+    points: 6,
+    col: 3,
+    created: getNow()
+  },
+]
